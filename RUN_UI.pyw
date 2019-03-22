c91b5d500e93b53d75d948db1939a3e196c7f0b0
Alejandro Prieto <alejandro.prieto@ree.es>
2019-03-22 10:33:59 +0100

    cambiado sort

---
+++ RUN_UI.pyw
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from Software.Interfaz.tcc_cne_UI import main
+
+if __name__ == '__main__':
+   main()
---
+++ Software/Interfaz/vistas/convertrecourse.bat
@@ -0,0 +1,3 @@
+cd 
+"C:\Python27\Lib\site-packages\PySide\pyside-rcc.exe" D:\TTC_CNE\Software\Interfaz\vistas\resources.qrc -o D:\TTC_CNE\Software\Interfaz\vistas\resources_rc.py
+pause
